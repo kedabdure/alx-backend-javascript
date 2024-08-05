a426c624fac9862e4c7e05156cc8e42831550389
kedabudre <abdurehimk77@gmail.com>
2024-08-05 23:15:28 +0300

    update file

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,4 @@
 export function taskFirst() {
-<<<<<<< HEAD
     const task = 'I prefer const when I can.';
     return task;
   }
@@ -14,19 +13,3 @@ export function taskFirst() {
   
     return combination;
   }
-=======
-  const task = 'I prefer const when I can.';
-  return task;
-}
-
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
->>>>>>> e81272542563cf586837a755af786c77a3b428e5
