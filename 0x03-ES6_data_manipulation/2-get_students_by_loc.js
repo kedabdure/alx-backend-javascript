5669614722a5f18c9d7557f04a3e8587cf2d59d7
kedabudre <abdurehimk77@gmail.com>
2024-08-12 20:48:09 +0300

    update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(data, city) {
-  const students = data.filter((item) => item.location === city)
-  return students
+  const students = data.filter((item) => item.location === city);
+  return students;
 }
