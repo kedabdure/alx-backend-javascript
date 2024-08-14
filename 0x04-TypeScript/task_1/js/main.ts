d8476f0355c85f5a2d984efdd2dafb8de8e97dde
kedabudre <abdurehimk77@gmail.com>
2024-08-14 23:14:16 +0300

    create new files for task_2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,19 +1,45 @@
 interface Teacher {
-  firstName: string;
-  lastName: string;
-  fullTimeEmployee: boolean;
-  yearOfExperience?: number;
-  location: string;
-  [key: string]: any;
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+
+  [propName: string]: any
 }
 
-// Create an example Teacher object
-const teacher3: Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
-  fullTimeEmployee: false,
-  location: 'London',
-  contract: false,
-};
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
 
-console.log(teacher3)
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface classInterface {
+  workOnHomework(): string,
+
+  displayName(): string
+}
+
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+  }
+
+
+  displayName(): string {
+      return this.firstName;
+  }
+
+  workOnHomework(): string {
+      return "Currently working";
+  }
+
+}
