f517dec40a2f93f906a8eed68b7b6860ac9b9248
kedabudre <abdurehimk77@gmail.com>
2024-10-02 04:38:29 +0200

    create 7-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,29 @@
 const express = require('express');
-
+const students = require('./3-read_file_async');
 const app = express();
-const PORT = 1245;
+const hostname = '127.0.0.1';
+const port = 1245;
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+app.get('/students', async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  await students(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+  }).catch((err) => res.write(err.message))
+    .finally(() => {
+      res.end();
+    });
 });
 
-module.exports = app;
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
